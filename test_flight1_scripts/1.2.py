4957d68f5d6e59ac264bd65e6a14842beeed187e
Marcus Eshleman <marcuseshleman@gmail.com>
2026-01-02 14:44:28 -0700

    Added second waypoint confirmation

--- test_flight1_scripts/1.2.py
+++ test_flight1_scripts/1.2.py
@@ -105,7 +105,7 @@ while True:
         break
 
     time.sleep(0.1)
-
+print("Aircraft has reached second waypoint")
 # landing aircraft back down
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0)
 
