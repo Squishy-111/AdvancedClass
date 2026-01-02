3a371bfc2c11fe97c89db2d906843f5471fd1151
Marcus Eshleman <marcuseshleman@gmail.com>
2026-01-02 14:38:50 -0700

    Changed to align with set_point behavior for waypoints

--- test_flight1_scripts/1.2.py
+++ test_flight1_scripts/1.2.py
@@ -52,42 +52,59 @@ print("Aircraft has reached cruising altitude")
 
 
 # takeoff and fly towards first waypoint (10m up and 10m ahead)
-the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10, the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_FRAME_LOCAL_NED, int(0b110111111000), 10, 0, -10, 0, 0, 0, 0, 0, 0, 0, 0))
-
-# acknowledgement message
-msg = the_connection.recv_match(type="COMMAND_ACK", blocking=True)
-print(msg)
-
-
+target_x, target_y, target_z = 10, 0, -10
 while True:
-  msg = the_connection.recv_match(type="LOCAL_POSITION_NED", blocking=True)
-  print(msg)
+    # resend setpoint at ~10 Hz
+    the_connection.mav.send(
+        mavutil.mavlink.MAVLink_set_position_target_local_ned_message(
+            0,  # time_boot_ms (0 is fine)
+            the_connection.target_system,
+            the_connection.target_component,
+            mavutil.mavlink.MAV_FRAME_LOCAL_NED,
+            int(0b110111111000),  # position enabled, vel/accel/yaw ignored
+            target_x, target_y, target_z,
+            0, 0, 0,
+            0, 0, 0,
+            0, 0
+        )
+    )
 
-  if msg.z <= -9.8 and msg.x >= 9.8:
-     break
+    msg = the_connection.recv_match(type="LOCAL_POSITION_NED", blocking=True)
+    print(msg)
+
+    if msg.z <= -9.8 and msg.x >= 9.8:
+        break
+
+    time.sleep(0.1)
 
 print("Aircraft has reached first waypoint")
 
 
-# fly towards second waypoint (10m right from current position)
-the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10, the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_FRAME_LOCAL_NED, int(0b110111111000), 10, 10, -10, 0, 0, 0, 0, 0, 0, 0, 0))
-
-# acknowledgement message
-msg = the_connection.recv_match(type="COMMAND_ACK", blocking=True)
-print(msg)
-
+target_x, target_y, target_z = 10, 10, -10
+type_mask = int(0b110111111000)
 
 while True:
-  msg = the_connection.recv_match(type="LOCAL_POSITION_NED", blocking=True)
-  print(msg)
+    the_connection.mav.send(
+        mavutil.mavlink.MAVLink_set_position_target_local_ned_message(
+            int(time.time() * 1000) & 0xFFFFFFFF, #This may need to change?
+            the_connection.target_system,
+            the_connection.target_component,
+            mavutil.mavlink.MAV_FRAME_LOCAL_NED,
+            type_mask,
+            target_x, target_y, target_z,
+            0, 0, 0,
+            0, 0, 0,
+            0, 0
+        )
+    )
 
-  if msg.z <= -9.8 and msg.x >= 9.8 and msg.y >= 9.8:
-     break
+    msg = the_connection.recv_match(type="LOCAL_POSITION_NED", blocking=True)
+    print(msg)
 
-print("Aircraft has reached second waypoint")
+    if msg and msg.x >= 9.8 and msg.y >= 9.8 and msg.z <= -9.8:
+        break
 
-
-print("initiating landing")
+    time.sleep(0.1)
 
 # landing aircraft back down
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0)
